e833f01aebaa8b4d1f0c8b41d8fdfbcdb1f59384
ham952 <ham952@hotmail.com>
2019-11-11 12:15:09 +0500

    Add files via upload

---
+++ Utils.py
@@ -0,0 +1,287 @@
+import numpy as np
+
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+
+    Arguments:
+    x -- A scalar or numpy array of any size
+
+    Return:
+    s -- sigmoid(x)
+    """
+
+    s = 1 / (1 + np.exp(-x))
+
+    return s
+
+
+def sigmoid_derivative(x):
+    """
+    Compute the gradient (also called the slope or derivative) of the sigmoid function with respect to its input x.
+    You can store the output of the sigmoid function into variables and then use it to calculate the gradient.
+
+    Arguments:
+    x -- A scalar or numpy array
+
+    Return:
+    ds -- Your computed gradient.
+    """
+
+    s = 1 / (1 + np.exp(-x))
+    ds = s * (1 - s)
+
+    return ds
+
+
+def image2vector(image):
+    """
+    Argument:
+    image -- a numpy array of shape (length, height, depth)
+
+    Returns:
+    v -- a vector of shape (length*height*depth, 1)
+    """
+
+    v = image.reshape((image.shape[0] * image.shape[1] * image.shape[2]), 1)
+
+
+    return v
+
+
+def normalizeRows(x):
+    """
+    Implement a function that normalizes each row of the matrix x (to have unit length).
+
+    Argument:
+    x -- A numpy matrix of shape (n, m)
+
+    Returns:
+    x -- The normalized (by row) numpy matrix. You are allowed to modify x.
+    """
+
+    # Compute x_norm as the norm 2 of x. Use np.linalg.norm(..., ord = 2, axis = ..., keepdims = True)
+    x_norm = np.linalg.norm(x, ord=2, axis=1, keepdims=True)
+
+    # Divide x by its norm.
+    x = x / x_norm
+
+
+    return x
+
+
+def softmax(x):
+    """Calculates the softmax for each row of the input x.
+
+    Your code should work for a row vector and also for matrices of shape (n, m).
+
+    Argument:
+    x -- A numpy matrix of shape (n,m)
+
+    Returns:
+    s -- A numpy matrix equal to the softmax of x, of shape (n,m)
+    """
+
+    # Apply exp() element-wise to x. Use np.exp(...).
+    x_exp = np.exp(x)
+
+    # Create a vector x_sum that sums each row of x_exp. Use np.sum(..., axis = 1, keepdims = True).
+    x_sum = np.sum(x_exp, axis=1, keepdims=True)
+
+    # Compute softmax(x) by dividing x_exp by x_sum. It should automatically use numpy broadcasting.
+    s = x_exp / x_sum
+
+
+    return s
+
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+
+    loss = np.sum(np.abs(y - yhat))
+
+
+    return loss
+
+
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+
+    loss = np.sum((y - yhat) ** 2)
+
+
+    return loss
+
+# initialize_with_zeros
+
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias)
+    """
+
+    ### START CODE HERE ###
+    w = np.zeros((dim,1))
+    b = 0
+    ### END CODE HERE ###
+
+    assert (w.shape == (dim, 1))
+    assert (isinstance(b, float) or isinstance(b, int))
+
+    return w, b
+
+# GRADED FUNCTION: optimize
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+
+    Tips:
+    You basically need to write down two steps and iterate through them:
+        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+        2) Update the parameters using gradient descent rule for w and b.
+    """
+
+    costs = []
+    m = X.shape[1]
+
+    for i in range(num_iterations):
+
+        # Cost and gradient calculation
+
+
+
+        # FORWARD PROPAGATION (FROM X TO COST)
+
+        A = 1 / (1 + np.exp(-(np.dot(w.T, X) + b)))  # compute activation
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A))) / m  # compute cost
+
+        # BACKWARD PROPAGATION (TO FIND GRAD)
+
+        dw = np.dot(X, (A - Y).T) / m
+        db = np.sum(A - Y) / m
+
+
+        # update w and b
+        w = w - learning_rate*dw
+        b = b - learning_rate*db
+
+
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+
+        # Print the cost every 100 training iterations
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" % (i, cost))
+
+    params = {"w": w,
+              "b": b}
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
+
+
+# GRADED FUNCTION: predict
+
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''
+
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    A = 1 / (1 + np.exp(-(np.dot(w.T, X) + b)))
+
+    for i in range(A.shape[1]):
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        if A[0][i] > 0.5:
+            Y_prediction[0][i] = 1
+        else:
+            pass
+
+    assert (Y_prediction.shape == (1, m))
+
+    return Y_prediction
+
+def predict_mod(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    Probability  -- Probabiity of Estimation
+    '''
+
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    A = 1 / (1 + np.exp(-(np.dot(w.T, X) + b)))
+
+    for i in range(A.shape[1]):
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        if A[0][i] > 0.5:
+            Y_prediction[0][i] = 1
+        else:
+            pass
+
+    assert (Y_prediction.shape == (1, m))
+    Probability = A * 100
+
+    return Y_prediction , Probability
